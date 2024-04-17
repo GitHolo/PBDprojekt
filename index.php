0123756cd2b9bf24b76eec82394398021c72555c
GitHolo <ooye2332@gmail.com>
2024-04-17 14:57:00 +0200

    header

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>PHPJabbers.com | Free Restaurant Website Template</title>
+		<title>Papa's Restaurants</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="shortcut icon" type="image/x-icon" href="./images/papas-pizzeria.jpg">
 		<script src="assets/js/jquery.min.js"></script>
 	</head>
 	<body class="is-preload">
